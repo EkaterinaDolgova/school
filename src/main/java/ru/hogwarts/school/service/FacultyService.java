e7ba864b226abb22cbfb3412dd9b95d2c6507191
katya <kotenokches@rambler.ru>
2022-06-02 08:16:04 +0500

    Четвертый коммит

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,5 +1,28 @@
 package ru.hogwarts.school.service;
 
-public class FacultyService {
+import ru.hogwarts.school.model.Faculty;
+import java.util.HashMap;
 
+public class FacultyService {
+    private final HashMap<Long, Faculty> facults = new HashMap<>();
+    private long key = 0;
+
+    public Faculty creatFaculty(Faculty faculty) {
+        faculty.setId(++key);
+        facults.put(key, faculty);
+        return faculty;
+    }
+
+    public Faculty readFaculty(long id) {
+        return facults.get(id);
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        facults.put(faculty.getId(), faculty);
+        return faculty;
+    }
+
+    public Faculty deleteFaculty(long id) {
+        return facults.remove(id);
+    }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -8,12 +8,25 @@ import java.util.HashMap;
 @Service
 public class StudentService {
     private final HashMap<Long, Student> students = new HashMap<>();
-    private long key=0;
+    private long key = 0;
 
-    public Student creatStudent(Student student){
+    public Student creatStudent(Student student) {
         student.setId(++key);
-        students.put(key,student);
+        students.put(key, student);
         return student;
+    }
+
+    public Student readStudent(long id) {
+        return students.get(id);
+    }
+
+    public Student editStudent(Student student) {
+        students.put(student.getId(), student);
+        return student;
+    }
+
+    public Student deleteStudent(long id) {
+        return students.remove(id);
     }
 
 }
